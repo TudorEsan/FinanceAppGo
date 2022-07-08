60fdc1a62e73cf5aabc7efcd96a9b49bcc15c7e5
Titus <tituse@gmail.com>
2022-07-08 15:57:18 +0300

    Updated Git ignore

--- server/controllers/networthController.go
+++ server/controllers/networthController.go
@@ -4,7 +4,6 @@ import (
 	"App/database"
 	"App/helpers"
 	"App/models"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,7 +23,6 @@ func InitNetWort() gin.HandlerFunc {
 
 func AddRecord() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("HERE?")
 		var record models.Record
 		if err := c.BindJSON(&record); err != nil {
 			helpers.ReturnError(c, http.StatusBadRequest, err)
@@ -49,4 +47,54 @@ func AddRecord() gin.HandlerFunc {
 			"netWorth": newNetWorth,
 		})
 	}
+}
+
+func GetNetWorth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user, err := helpers.GetUserFromContext(c)
+		if err != nil {
+			helpers.ReturnError(c, http.StatusInternalServerError, err)
+			return
+		}
+		netWorth, err := helpers.GetNetWorth(user.ID)
+		if err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"netWorth": netWorth})
+
+	}
+}
+
+func DeleteRecord() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user, err := helpers.GetUserFromContext(c)
+		if err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+			return
+		}
+		var body models.DeleteRecordBody
+		if err = c.BindJSON(&body); err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+			return
+		}
+		if err := validate.Struct(body); err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+			return
+		}
+		if err != nil {
+			helpers.ReturnError(c, http.StatusInternalServerError, err)
+			return
+		}
+		id, err := primitive.ObjectIDFromHex(body.Id)
+		if err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+		}
+		netWorth, err := helpers.DeleteRecord(user.ID, id)
+		if err != nil {
+			helpers.ReturnError(c, http.StatusBadRequest, err)
+		}
+		c.JSON(http.StatusOK, gin.H{"netWorth": netWorth})
+	}
+
 }
--- server/models/networthModel.go
+++ server/models/networthModel.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"sync"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -13,21 +14,21 @@ type Diversificationable interface {
 
 type NetWorth struct {
 	Id      primitive.ObjectID `json:"id" bson:"_id"`
-	UserId  primitive.ObjectID `json:"userId" bson:"userId" validate:"required"`
+	UserId  primitive.ObjectID `json:"userId" bson:"userId" binding:"required"`
 	Records []Record           `json:"records" bson:"records"`
 }
 
 type Record struct {
 	Id                    primitive.ObjectID `json:"id" bson:"id"`
-	Date                  time.Time          `json:"date" bson:"date" validate:"required"`
-	CurrentMoney          float32            `json:"currentMoney" bson:"currentMoney" validate:"required"`
-	Stocks                Stocks             `json:"stocks" bson:"stocks"`
-	Cryptos               Cryptos            `json:"cryptos" bson:"cryptos"`
-	StockDiversification  []Diversification  `json:"stockDiversification"`
+	Date                  time.Time          `json:"date" bson:"date" binding:"required"`
+	InvestedAmount        float32            `json:"investedAmount,omitempty" bson:"investedAmount"`
+	Stocks                Stocks             `json:"stocks" bson:"stocks" binding:"required,dive"`
+	Cryptos               Cryptos            `json:"cryptos" bson:"cryptos" binding:"required"`
+	StockDiversification  []Diversification  `json:"stockDiversification" `
 	CryptoDiversification []Diversification  `json:"cryptoDiversification"`
-	CryptoWorth           float32            `json:"cryptoWorth,omitempty"`
-	StockWorth            float32            `json:"stockWorth,omitempty"`
-	Liquidity             float32            `json:"liquidity" bson:"liquidity" validate:"min=0"`
+	CryptosValue          float32            `json:"cryptosValue,omitempty" bson:"cryptosValue"`
+	StocksValue           float32            `json:"stocksValue,omitempty" bson:"stocksValue"`
+	Liquidity             *float32           `json:"liquidity" bson:"liquidity" binding:"required,min=0"`
 }
 
 type Diversification struct {
@@ -36,51 +37,75 @@ type Diversification struct {
 }
 
 type Stock struct {
-	Name     string  `json:"name" bson:"name"`
-	Symbol   string  `json:"symbol" bson:"symbol"`
-	Shares   float32 `json:"shares" bson:"shares"`
-	ValuedAt float32 `json:"valuedAt" bson:"valuedAt"`
+	Name     string   `json:"name" bson:"name" binding:"required"`
+	Symbol   string   `json:"symbol" bson:"symbol" binding:"required"`
+	Shares   float32  `json:"shares" bson:"shares" binding:"required,min=0"`
+	ValuedAt *float32 `json:"valuedAt" bson:"valuedAt" binding:"required,min=0"`
 }
 
 type Crypto struct {
-	Name     string  `json:"name" bson:"name"`
-	Symbol   string  `json:"symbol" bson:"symbol"`
-	Coins    float32 `json:"coins" bson:"coins"`
-	ValuedAt float32 `json:"valuedAt" bson:"valuedAt"`
+	Name     string   `json:"name" bson:"name" binding:"required"`
+	Symbol   string   `json:"symbol" bson:"symbol" binding:"required"`
+	Coins    float32  `json:"coins" bson:"coins" binding:"required,min=0"`
+	ValuedAt *float32 `json:"valuedAt" bson:"valuedAt" binding:"required,min=0"`
 }
 
 type Stocks []Stock
 type Cryptos []Crypto
 
-func (stocks Stocks) GetStocksValue() (sum float32) {
+type DeleteRecordBody struct {
+	Id string `json:"id" binding:"required"`
+}
+
+func (stocks Stocks) GetValue() (sum float32) {
 	for _, stock := range stocks {
-		sum += stock.ValuedAt
+		sum += *stock.ValuedAt * stock.Shares
 	}
 	return
 }
-func (cryptos Cryptos) GetStocksValue() (sum float32) {
-	for _, stock := range cryptos {
-		sum += stock.ValuedAt
+func (cryptos Cryptos) GetValue() (sum float32) {
+	for _, crypto := range cryptos {
+		sum += *crypto.ValuedAt * crypto.Coins
 	}
 	return
+}
+func (stock Stock) GetValue() (sum float32) {
+	return *stock.ValuedAt * stock.Shares
+}
+func (crypto Crypto) GetValue() (sum float32) {
+	return *crypto.ValuedAt * crypto.Coins
 }
 
-func (record Record) GetStockDiversification() (diversification []Diversification) {
-	diversification = make([]Diversification, len(record.Stocks))
-	stocksWorth := record.Cryptos.GetStocksValue()
+func (record Record) GetStockDiversification(stocksValue float32) (diversification []Diversification) {
+	diversification = make([]Diversification, 0, len(record.Stocks))
 	for _, stock := range record.Stocks {
-		div := Diversification{stock.Symbol, stock.ValuedAt / stocksWorth}
+		div := Diversification{stock.Symbol, stock.GetValue() / stocksValue}
 		diversification = append(diversification, div)
 	}
 	return
 }
 
-func (record Record) GetCryptoDiversification() (diversification []Diversification) {
-	diversification = make([]Diversification, len(record.Stocks))
-	cyptoWorth := record.Cryptos.GetStocksValue()
-	for _, crypto := range record.Stocks {
-		div := Diversification{crypto.Symbol, crypto.ValuedAt / cyptoWorth}
+func (record Record) GetCryptoDiversification(cryptosValue float32) (diversification []Diversification) {
+	diversification = make([]Diversification, 0, len(record.Cryptos))
+	for _, crypto := range record.Cryptos {
+		div := Diversification{crypto.Symbol, crypto.GetValue() / cryptosValue}
 		diversification = append(diversification, div)
 	}
 	return
+}
+
+func (record *Record) GenerateStatistics() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		record.StocksValue = record.Stocks.GetValue()
+		record.StockDiversification = record.GetStockDiversification(record.StocksValue)
+		wg.Done()
+	}()
+	go func() {
+		record.CryptosValue = record.Cryptos.GetValue()
+		record.CryptoDiversification = record.GetCryptoDiversification(record.CryptosValue)
+		wg.Done()
+	}()
+	wg.Wait()
 }

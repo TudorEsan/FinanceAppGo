87252082b34de8ccd538f0fada443cf74e235a4b
Tudor <tudor.esan@icloud.com>
2022-08-18 18:27:42 +0300

    Added Pie Legend

--- client/src/components/Charts/PieChart.tsx
+++ client/src/components/Charts/PieChart.tsx
@@ -1,20 +1,27 @@
+import { fabClasses } from "@mui/material";
 import { Box } from "@mui/system";
 import { ResponsivePie } from "@nivo/pie";
 import { BasicTooltip } from "@nivo/tooltip";
+import { createFalse } from "typescript";
 import { useMobile } from "../../hooks/useMobile";
 import { IDiversification } from "../../types/record";
 
 interface IProps {
   data: IDiversification[];
   enableArcLinkLabels?: boolean;
+  showLegend?: boolean;
 }
 
-export const MyPie = ({ data, enableArcLinkLabels = true }: IProps) => {
+export const MyPie = ({
+  data,
+  showLegend = false,
+  enableArcLinkLabels = true,
+}: IProps) => {
   const isMobile = useMobile();
   return (
     <Box
       height={isMobile ? 250 : 300}
-      width="100%"
+      // width="100%"
       maxWidth={400}
       margin="auto"
     >
@@ -22,10 +29,30 @@ export const MyPie = ({ data, enableArcLinkLabels = true }: IProps) => {
       <ResponsivePie
         colors={{ scheme: "dark2" }}
         value="percent"
+        legends={
+          showLegend
+            ? [
+                {
+                  anchor: "bottom",
+                  direction: "row",
+                  translateX: 0,
+                  translateY: 50,
+                  itemWidth: 75,
+                  itemHeight: 20,
+                  itemsSpacing: 5,
+                  symbolSize: 20,
+                  itemDirection: "left-to-right",
+                  itemTextColor: "white",
+                },
+              ]
+            : []
+        }
         id="symbol"
         data={data}
         margin={
-          enableArcLinkLabels ? { right: 90, left: 90 } : { right: 0, left: 0 }
+          enableArcLinkLabels
+            ? { right: 90, left: 90, bottom: 90 }
+            : { right: 0, left: 0, bottom: 90 }
         }
         innerRadius={0.5}
         padAngle={2}

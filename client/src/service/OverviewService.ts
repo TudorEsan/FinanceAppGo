f2f69d499a4290fd4f55e8e412313f8b7dc606fb
Tudor <tudor.esan@icloud.com>
2022-09-15 12:35:10 +0300

    Adjusted limit for networth overview

--- client/src/service/OverviewService.ts
+++ client/src/service/OverviewService.ts
@@ -3,7 +3,9 @@ import axios from "../axiosConfig";
 import { INetWorth, IOverview } from "../types/overview";
 import { serverUrl } from "./general";
 
-export const getNetWorthOverviewReq = async () => {
-  const resp = await axios.get(serverUrl() + `/overview/networth`);
+export const getNetWorthOverviewReq = async (limit: number) => {
+  const resp = await axios.get(serverUrl() + `/overview/networth`, {
+    params: { limit },
+  });
   return resp.data.overview as IOverview;
 };

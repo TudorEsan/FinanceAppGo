d0453f6fb1ec86555512260a9b5d55420efb50a1
Tudor <tudor.esan@icloud.com>
2022-11-01 11:19:36 +0200

    Broker Service Wallet + Staking Assets Analysation

---
+++ BrokerService/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/TudorEsan/FinanceAppGo/BrokerService/service"
+)
+
+
+func main() {
+	// binanceS := service.NewBinanceService(apiKey, secretKey)
+	// assets, err := binanceS.GetAssets()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(assets)
+}
